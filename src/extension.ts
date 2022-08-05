07f580f21b9910d0b5f027ccd40b9e2bd97fe375
Anna Zhukova <28830446+bindreams@users.noreply.github.com>
2022-08-05 22:49:24 +0300

    Remove console logs

--- src/extension.ts
+++ src/extension.ts
@@ -14,9 +14,6 @@ var isEmpty = (document: vscode.TextDocument) => {
 }
 
 export function activate(context: vscode.ExtensionContext) {
-	const mappings: Array<SnippetMapping> = vscode.workspace.getConfiguration('autoSnippet').get('snippets', []);
-	console.log(mappings);
-
 	let processSnippets = vscode.workspace.onDidOpenTextDocument(document => {
 		if (document?.uri?.scheme !== 'file' || !isEmpty(document)) {
 			return;
@@ -24,12 +21,8 @@ export function activate(context: vscode.ExtensionContext) {
 
 		const mappings: Array<SnippetMapping> = vscode.workspace.getConfiguration('autoSnippet').get('snippets', []);
 		const filename = document.fileName;
-		console.log("Opened empty file " + filename);
 
-		console.log(mappings);
 		mappings.forEach((mapping: SnippetMapping) => {
-			console.log(mapping);
-
 			var languageMatch = mapping.language && mapping.language === document.languageId;
 			var nameMatch = mapping.pattern && micromatch.isMatch(filename, mapping.pattern);
 			var regexMatch = false;
@@ -39,10 +32,6 @@ export function activate(context: vscode.ExtensionContext) {
 				regexMatch = !!filename.match(regex);
 			}
 
-			if (languageMatch) console.log(" Matched language " + document.languageId);
-			if (nameMatch) console.log(" Matched pattern " + mapping.pattern);
-			if (regexMatch) console.log(" Matched regex " + mapping.regex);
-
 			if (!languageMatch && !nameMatch && !regexMatch) {
 				return;
 			}
@@ -50,8 +39,6 @@ export function activate(context: vscode.ExtensionContext) {
 			vscode.window.showTextDocument(document, 0, false);
 			vscode.commands.executeCommand('editor.action.selectAll').then(() => {
 				if (mapping.snippet) {
-					console.log(" Inserting snippet " + mapping.snippet);
-
 					var insertedTimeout = setTimeout(() => {
 						vscode.window.showErrorMessage("Missing, empty or invalid snippet: " + mapping.snippet);
 					}, 1000);
@@ -60,9 +47,6 @@ export function activate(context: vscode.ExtensionContext) {
 						var editor = vscode.window.activeTextEditor;
 						if (editor && editor.document && editor.document.getText() === "") {
 							vscode.window.showErrorMessage("Missing, empty or invalid snippet: " + mapping.snippet);
-							console.log("  Nothing inserted. Does snippet exist?");
-						} else {
-							console.log("  Inserted");
 						}
 
 						clearTimeout(insertedTimeout);
@@ -71,7 +55,6 @@ export function activate(context: vscode.ExtensionContext) {
 
 				if (mapping.commands) {
 					mapping.commands.forEach(command => {
-						console.log(" Running command: " + command);
 						vscode.commands.executeCommand(command);
 					});
 				}
